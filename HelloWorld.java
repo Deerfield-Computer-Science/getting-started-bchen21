8e9abc210e00cde0757bc585318502ad686af8c0
Ben Chen <bchen21@deerfield.edu>
2020-04-02 10:15:05 -0400

    This is my first commit! 10:14:41am on 4/2/20

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 import acm.program.*;
 
-public class HelloWorld extends ConsoleProgram{
+public class HelloWorld extends ConsoleProgram {
 	public void run() {
 		println("Hello World");
+		println("I do wish I could go back to school :/");
 	}
 }
